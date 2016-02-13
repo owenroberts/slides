c42d249916c4f52b4361d05c0fdbb52e04a5fdbb
Owen Roberts <owenroberts@Owens-iMac.local>
2016-02-13 15:00:29 -0500

    updates slides files

--- slides.js
+++ slides.js
@@ -6,10 +6,13 @@ $(document).ready( function() {
 	var outlineBtn = $('<button>').attr('id', 'outlineBtn');
 
 	var slides = $('.slide');
+	var isslides = false;
 	var slideNumber = 0;
 	var numSlides = slides.length;
 	var scrolling = false;
 	var noscroll = false;
+
+	var h = window.innerHeight;
 
 	slidesBtn.text('Slides');
 	outlineBtn.text('Outline');
@@ -17,28 +20,62 @@ $(document).ready( function() {
 	$(container).append(slidesBtn);
 	$(container).append(outlineBtn);
 
+
+
 	var setSlides = function() {
-		$(container).addClass('slides');
-		$(container).removeClass('outline');
-		noscroll = false;
+		if (!isslides) {
+			isslides = true;
+			$(container).addClass('slides');
+			$(container).removeClass('outline');
+			$('#defaultCanvas0').show();
+			noscroll = false;
+	
+			slides.each(function() {
+				var elemheight = $(this).height();
+				//console.log(this, elemheight);
+				if (elemheight > h) {
+					$(this).addClass("long");
+					console.log(this);
+				}
+				else {
+					$(this).css({height:h});
+					var firstchild = $(this).children()[0];
+					$(firstchild).css({marginTop:(h-elemheight)/4});
+				}	
+			});
+		}
 	};
 	$(slidesBtn).on('click', setSlides);
 	
 	var setOutline = function() {
-		$(container).removeClass('slides');
-		$(container).addClass('outline');
-		noscroll = true;
+		if (isslides) {
+			isslides = false;	
+			$(container).removeClass('slides');
+			$(container).addClass('outline');
+			$('#defaultCanvas0').hide();
+			noscroll = true;
+			slides.each(function() {
+				$(this).css({height:"auto"});
+				var firstchild = $(this).children()[0];
+				$(firstchild).css({marginTop:"auto"});
+			});
+		}
 	};
 	$(outlineBtn).on('click', setOutline);
 	setOutline();
+	setSlides();
 
 	var setSlideNumber = function() {
-		slideNumber = 0;
-		for ( var i = 0; i < slides.length; i++) {
-			if ( $(document).scrollTop() > $(slides[i]).offset().top ) {
-				slideNumber++;
+		var longslide = $(slides[slideNumber]).attr('class');
+		if (longslide.indexOf("long") == -1) {
+			slideNumber = 0;
+			for ( var i = 0; i < slides.length; i++) {
+				if ( $(document).scrollTop() > $(slides[i]).offset().top + h/4) {
+					slideNumber++;
+				}
 			}
-		}
+			return true;
+		} else return false;
 	};
 
 	var scrollToSlide = function() {
@@ -83,9 +120,8 @@ $(document).ready( function() {
 	});
 
 	$(document).on("wheel", function() {
-		//console.log("scrolling");
-		setSlideNumber();
-		if (!scrolling) setTimeout(scrollToSlide, 2000);
+		var slideOkay = setSlideNumber();
+		if (!scrolling && slideOkay) setTimeout(scrollToSlide, 2000);
 		scrolling = true;
 	});
 
